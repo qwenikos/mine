01c7fc20c7598447f77804bd1ae628e1ac385814
nikos <nikos@nikos-bio>
2015-12-11 23:29:21 +0200

    first edition

---
+++ test_python/src/test_python.py
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "nikos"
+__date__ = "$Dec 11, 2015 11:18:45 PM$"
+
+if __name__ == "__main__":
+    i=10
+    dd="nikos";
+    print "Hello World";
+    if (i==10) :print dd;
+    

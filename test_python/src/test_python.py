eda6dcd6467ad4d1f93901d0ac99345a500e0f3b
nikos <nikos@nikos-bio>
2015-12-16 10:56:31 +0200

    first edition

--- test_python/src/test_python.py
+++ test_python/src/test_python.py
@@ -3,24 +3,37 @@ __date__ = "$Dec 11, 2015 11:18:45 PM$"
 import csv;
 from collections import defaultdict, Counter
 def test1():
-    if __name__ == "__main__":
-        i=10
-    dd="nikos";
-    if (i==10) :
-        print dd;
-    days=[1,2,3,4]
-    matrix=[[1,2,4],days]
-    females=set([1,2,3,4,4])
-    males=set([1,4,6,6,7,8])
-    print females &males  #set([1, 4])
-    print females  #set([1, 2, 3, 4])
-    ageDict={'homer':36,'marge':34,'lisa':8,'bart':10,12:2}
-    print(ageDict['lisa']) #8
-    print(ageDict[12]) #8
-    x='22'
-    y='78'
-    print(x+y)
+ 
+    tuple1=(1,(2,3))
+    tuple2=(1,(2,4))
+    tuple3=(2,(4,6))
+    tuple4=(1,(2,3))
+    nlist=[tuple1,tuple2,tuple3,tuple4]
+
+    print nlist[1][1]
+
+    
+#    if __name__ == "__main__":
+#        i=10
+#    dd="nikos";
+#    if (i==10) :
+#        print dd;
+#    days=[1,2,3,4]
+ 
+#    matrix=[[1,2,4],days]
+#    females=set([1,2,3,4,4])
+#    males=set([1,4,6,6,7,8])
+#    print females &males  #set([1, 4])
+#    print females  #set([1, 2, 3, 4])
+#    ageDict={'homer':36,'marge':34,'lisa':8,'bart':10,12:2}
+#    print(ageDict['lisa']) #8
+#    print(ageDict[12]) #8
+#    x='22'
+#    y='78'
+#    print(x+y)
     return
+
+
 def filter1():
     linc_file_name='/home/nikos/biothesis/data/ensemble_linc_genes.csv'
     protein_coding_file_name='/home/nikos/biothesis/data/ensemble_protein_coding_gene.csv'
@@ -88,6 +101,7 @@ def filter2():
 def filter3():
     unique_onlycount=0;
     rec_count=0;
+    nlist=[]
     linc_file_name='/home/nikos/biothesis/data/ensemble_linc_genes.csv'
     protein_coding_file_name='/home/nikos/biothesis/data/ensemble_protein_coding_gene.csv'
     map_genes = defaultdict(Counter)#a dict of counters to count 
@@ -98,6 +112,8 @@ def filter3():
         rec_count+=1;
         gene_id,trans_id,chrom,gene_start,gent_end,strand,tss,tsl = line.split(",")
         map_genes[(gene_id)][(trans_id,chrom,gene_start,gent_end,strand,tss,tsl)] +=1
+        
+        nlist.append([(gene_id),(trans_id,chrom,gene_start,gent_end,strand,tss,tsl)])
     
     for (gene_id), counter in map_genes.items():
         if(len(counter)==1):
@@ -105,11 +121,12 @@ def filter3():
 #            print gene_id
             trans_id,chrom,gene_start,gent_end,strand,tss,tsl = list(counter.keys())[0]
         # print all the repetitions
-            print(gene_id,trans_id,chrom,gene_start,gent_end,strand,tss,tsl)
-            
+#            print(gene_id,trans_id,chrom,gene_start,gent_end,strand,tss,tsl)
+    print nlist[2]
     print unique_onlycount
     print rec_count
     return
+#test1()
 #filter1()
 #filter2()
 filter3()
